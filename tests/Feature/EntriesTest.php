c538e0d18c15075ed004d1e9089347c8da530c11
Carston Henderson <carstonhenderson@icloud.com>
2019-04-11 21:39:31 -0600

    add entries feature test class with create and store tests

---
+++ tests/Feature/EntriesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Entry;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+class EntriesTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_create_an_entry()
+    {        
+        $this->get('/entries/create')->assertOk();
+    }
+
+    /** @test */
+    public function a_user_can_store_an_entry()
+    {
+        $this->withoutExceptionHandling();
+        
+        $attributes = factory(Entry::class)->raw();
+
+        $this->post('/entries', $attributes);
+        
+        $this->assertDatabaseHas('entries', $attributes);
+    }
+}
