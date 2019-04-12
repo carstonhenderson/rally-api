9b7501eceb3e1289ed6239ae0611b31ca4595f39
Carston Henderson <carstonhenderson@icloud.com>
2019-04-11 21:40:47 -0600

    add entries controller with 'index', 'create', and 'store' methods

---
+++ app/Http/Controllers/EntriesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entry;
+use Illuminate\Http\Request;
+
+class EntriesController extends Controller
+{
+    public function index()
+    {
+        return Entry::all();
+    }
+
+    public function create()
+    {
+        return view('entries.create');
+    }
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'mood' => 'required',
+            'notes' => 'nullable'
+        ]);
+
+        Entry::create($attributes);
+
+        return redirect('/entries');
+    }
+
+    public function show(Entry $entry)
+    {
+        //
+    }
+
+    public function edit(Entry $entry)
+    {
+        //
+    }
+
+    public function update(Request $request, Entry $entry)
+    {
+        //
+    }
+
+    public function destroy(Entry $entry)
+    {
+        //
+    }
+}
