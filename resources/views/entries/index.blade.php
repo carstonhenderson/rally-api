20c77de470aa77ebcd8b2767c844ab187e106431
Carston Henderson <carstonhenderson@icloud.com>
2019-05-03 14:26:00 -0600

    Update entries table design and layout

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
+@section('title', 'Entries')
+
 @section('content')
-    <table>
+    <div class="flex justify-between mb-8">
+        <h5 class="text-grey-dark">{{ $entries->count() }} <span class="font-normal">entries</span></h5>
+
+        <a 
+            href="/entries/create" 
+            class="text-blue border border-blue py-2 px-4 mb-2 rounded shadow float-right no-underline bg-transparent hover:bg-blue hover:text-white"
+        >Add entry</a>
+    </div>
+
+    <table class="table-fixed w-screen -mx-4">
         <thead>
-            <tr>
-                <th>#</th>
-                <th>Mood</th>
-                <th>Notes</th>
-                <th>Date</th>
+            <tr class="border-b border-grey pb-4">
+                <th class="pb-4 pl-4 text-left">Mood</th>
+                <th class="pb-4">Notes</th>
+                <th class="pb-4 pr-4 text-right">Date</th>
             </tr>
         </thead>
 
         <tbody>
             @foreach($entries as $entry)
-                <tr>
-                    <td>{{ $entry->id }}</td>
-                    <td>{{ $entry->mood }}</td>
-                    <td>{{ $entry->notes }}</td>
-                    <td>{{ $entry->created_at }}</td>
+                <tr class="border-b border-grey-light">
+                    <td class="py-4 pl-4">{{ $entry->emoji }}</td>
+                    <td class="py-4 truncate">
+                        <a href="/entries/{{ $entry->id }}" class="no-underline text-blue">{{ $entry->notes }}</a>
+                    </td>
+                    <td class="py-4 pr-4 text-right">{{ $entry->created_at->format('D, j M') }}</td>
                 </tr>
             @endforeach
         </tbody>
