9921fe9b8352d345244e9cfe7aef9c4aae6013b6
Carston Henderson <carstonhenderson@icloud.com>
2019-05-06 22:02:03 -0600

    Add entries 'show' view

---
+++ resources/views/entries/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', "View Entry")
+
+@section('content')
+    <div class="flex justify-between mb-8">
+        <a 
+            href="/entries/{{ $entry->id }}/edit"
+            class="text-blue border border-blue py-2 px-4 mb-2 rounded shadow float-right no-underline bg-transparent hover:bg-blue hover:text-white"
+        >Edit entry</a>
+
+        <form action="/entries/{{ $entry->id }}" method="POST">
+            @method('DELETE')
+            @csrf
+
+            <button 
+                type="submit"
+                class="text-red border border-red py-2 px-4 mb-2 rounded shadow float-right no-underline bg-transparent hover:bg-red hover:text-white"
+            >Delete entry</button>
+        </form>
+    </div>
+
+    <h3>{{ $entry->emoji }}</h3>
+    <h4>{{ $entry->created_at->format('D, j M') }}</h4>
+    <p>{{ $entry->notes }}</p>
+@endsection
