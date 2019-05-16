c0a472f9d1295a62637baf933d66f1b2edf73731
Carston Henderson <carstonhenderson@icloud.com>
2019-05-16 15:10:26 -0600

    Update style for entries index view

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -4,7 +4,7 @@
 
 @if (session('status'))
     @section('message')
-        <div class="-mt-4 -mx-4 bg-purple p-4 text-white text-center">{{ session('status') }}</div>
+        <div class="mb-4 -mt-4 -mx-4 bg-purple p-4 text-white text-center">{{ session('status') }}</div>
     @endsection
 @endif
 
@@ -18,25 +18,29 @@
         >Add entry</a>
     </div>
 
-    <table class="table-fixed w-screen -mx-4">
-        <thead>
-            <tr class="border-b border-grey pb-4">
-                <th class="pb-4 pl-4 text-left">Mood</th>
-                <th class="pb-4">Notes</th>
-                <th class="pb-4 pr-4 text-right">Date</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            @foreach($entries as $entry)
-                <tr class="border-b border-grey-light">
-                    <td class="py-4 pl-4">{{ $entry->emoji }}</td>
-                    <td class="py-4 truncate">
-                        <a href="/entries/{{ $entry->id }}" class="no-underline text-blue">{{ $entry->notes }}</a>
-                    </td>
-                    <td class="py-4 pr-4 text-right">{{ $entry->created_at->format('D, j M') }}</td>
+    @if ($entries->count() == 0)
+        <div class="text-center">You don't have any entries yet!</div>
+    @else
+        <table class="table-fixed w-screen -mx-4">
+            <thead>
+                <tr class="border-b border-grey pb-4">
+                    <th class="pb-4 pl-4 text-left">Mood</th>
+                    <th class="pb-4">Notes</th>
+                    <th class="pb-4 pr-4 text-right">Date</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+
+            <tbody>
+                @foreach($entries as $entry)
+                    <tr class="border-b border-grey-light">
+                        <td class="py-4 pl-4">{{ $entry->emoji }}</td>
+                        <td class="py-4 truncate">
+                            <a href="/entries/{{ $entry->id }}" class="no-underline text-blue">{{ $entry->notes ?? 'No notes' }}</a>
+                        </td>
+                        <td class="py-4 pr-4 text-right">{{ $entry->created_at->format('D, j M') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 @endsection
