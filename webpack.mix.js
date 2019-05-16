35ef2758f187edd418adf5df67d443d98eff7c4d
Carston Henderson <carstonhenderson@icloud.com>
2019-05-15 21:03:38 -0600

    Upgrade Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ require('laravel-mix-tailwind');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .tailwind();
+    .tailwind('./tailwind.config.js');
