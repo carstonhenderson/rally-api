59b9f1c9ffb59f9e42d012598dcaa870eeb2bbf5
Carston Henderson <carstonhenderson@icloud.com>
2019-05-17 16:35:23 -0600

    Add auth middleware to entry routes

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('entries', 'EntriesController');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('entries', 'EntriesController');
+});
