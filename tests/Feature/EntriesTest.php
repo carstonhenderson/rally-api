ae575a4fcf814467043f56f83677e78d1d57d8e2
Carston Henderson <carstonhenderson@icloud.com>
2019-05-17 16:37:47 -0600

    Add user authorization and tests

--- tests/Feature/EntriesTest.php
+++ tests/Feature/EntriesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Entry;
+Use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,14 +15,18 @@ class EntriesTest extends TestCase
 
     /** @test */
     public function a_user_can_create_an_entry()
-    {        
+    {
+        $this->actingAs(factory(User::class)->create());
+
         $this->get('/entries/create')->assertOk();
     }
 
     /** @test */
     public function a_user_can_store_an_entry()
-    {        
-        $attributes = factory(Entry::class)->raw();
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $attributes = collect(factory(Entry::class)->raw())->forget('user_id')->toArray();
 
         $this->post('/entries', $attributes);
         
@@ -31,13 +36,17 @@ class EntriesTest extends TestCase
     /** @test */
     public function a_user_can_view_entries()
     {
+        $this->actingAs(factory(User::class)->create());
+
         $this->get('/entries')->assertOk();
     }
 
     /** @test */
     public function a_user_can_view_an_entry()
     {
-        $entry = factory(Entry::class)->create();
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = auth()->user()->entries()->save(factory(Entry::class)->make());
 
         $this->get("/entries/{$entry->id}")->assertOk();
     }
@@ -45,7 +54,9 @@ class EntriesTest extends TestCase
     /** @test */
     public function a_user_can_edit_an_entry()
     {
-        $entry = factory(Entry::class)->create();
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = auth()->user()->entries()->save(factory(Entry::class)->make());
 
         $this->get("/entries/{$entry->id}/edit")->assertOk();
     }
@@ -53,9 +64,11 @@ class EntriesTest extends TestCase
     /** @test */
     public function a_user_can_udpate_an_entry()
     {
-        $entry = factory(Entry::class)->create();
+        $this->actingAs(factory(User::class)->create());
 
-        $attributes = factory(Entry::class)->raw();
+        $entry = auth()->user()->entries()->save(factory(Entry::class)->make());
+
+        $attributes = collect(factory(Entry::class)->raw())->forget('user_id')->toArray();
 
         $this->patch("/entries/{$entry->id}", $attributes);
 
@@ -65,10 +78,54 @@ class EntriesTest extends TestCase
     /** @test */
     public function a_user_can_delete_an_entry()
     {
-        $entry = factory(Entry::class)->create();
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = auth()->user()->entries()->save(factory(Entry::class)->make());
 
         $this->delete("/entries/{$entry->id}");
 
         $this->assertDatabaseMissing('entries', $entry->getAttributes());
+    }
+
+    /** @test */
+    public function a_user_can_only_view_their_own_entries()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = factory(Entry::class)->create();
+
+        $this->get("/entries/{$entry->id}")->assertForbidden();
+    }
+
+    /** @test */
+    public function a_user_can_only_edit_their_own_entries()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = factory(Entry::class)->create();
+
+        $this->get("/entries/{$entry->id}/edit")->assertForbidden();
+    }
+
+    /** @test */
+    public function a_user_can_only_udpate_their_own_entries()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = factory(Entry::class)->create();
+
+        $attributes = collect(factory(Entry::class)->raw())->forget('user_id')->toArray();
+
+        $this->patch("/entries/{$entry->id}", $attributes)->assertForbidden();
+    }
+
+    /** @test */
+    public function a_user_can_only_delete_their_own_entries()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $entry = factory(Entry::class)->create();
+
+        $this->delete("/entries/{$entry->id}")->assertForbidden();
     }
 }
