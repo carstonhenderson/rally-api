7a70717886b192820a31fc0633b6f41bba511bf6
Carston Henderson <carstonhenderson@icloud.com>
2019-05-22 14:25:53 -0600

    Add insights index view

---
+++ resources/views/insights/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Insights')
+
+@section('content')
+    <div class="h-full flex items-center justify-between mb-8">
+        <h2 class="w-full text-center">Insights are coming soon... 🔨</h2>
+    </div>
+@endsection
